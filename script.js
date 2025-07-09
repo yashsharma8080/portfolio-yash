15c05b012482dbf8ae0074544ef77485ef5eb433
Yash Sharma <thesharmaji8080@gmail.com>
2025-07-09 17:21:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Simple scroll animation
+document.querySelectorAll("nav a").forEach(link => {
+  link.addEventListener("click", e => {
+    e.preventDefault();
+    const target = document.querySelector(link.getAttribute("href"));
+    target.scrollIntoView({ behavior: "smooth" });
+  });
+});
